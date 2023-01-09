5828d75304db1391a62e1be2e3e599d279658334
Brent Haub <brent.haub@spyderbat.com>
2023-01-09 12:45:00 -0600

    changing doc style to bizstyle

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'spyctl'
-copyright = '2023, Brent Haub, Kyle Smith-Hanna'
-author = 'Brent Haub, Kyle Smith-Hanna'
-release = '0.0.24'
+project = "spyctl"
+copyright = "2023, Brent Haub, Kyle Smith-Hanna"
+author = "Brent Haub, Kyle Smith-Hanna"
+release = "0.0.24"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "bizstyle"
+html_static_path = ["_static"]
