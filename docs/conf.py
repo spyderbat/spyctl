e3883d75cd68de46e42ae18fcb89afe0b9bc948c
Brent Haub <brent.haub@spyderbat.com>
2023-01-11 10:05:33 -0600

    dark mode docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx_rtd_dark_mode'
 ]
 
 templates_path = ["_templates"]
@@ -31,5 +32,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
