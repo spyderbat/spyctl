2a80b038794e1951668cd62e94e29590ef835892
Brent Haub <brent.haub@spyderbat.com>
2023-03-31 15:22:43 -0500

    increment version

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ project = "Spyctl"
 copyright = "2023, SPYDERBAT, Inc., All Rights Reserved"
 author = "Spyderbat"
 release = "0.9"
-version = "0.9.23"
+version = "0.9.24"
 
 
 # -- General configuration ---------------------------------------------------
