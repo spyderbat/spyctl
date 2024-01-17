3aef11d99eada53bd2ee153feb2ba6f552e94dd2
spyder-ruhika <ruhika.bulani@spyderbat.com>
2024-01-17 22:02:03 +0530

    appropriate fields for tabular o/p

--- spyctl/resources/replicasets.py
+++ spyctl/resources/replicasets.py
@@ -7,8 +7,7 @@ import spyctl.spyctl_lib as lib
 SUMMARY_HEADERS = [
     "NAME",
     "D/C/R",
-    # "UP TO DATE",
-    # "AVAILABLE",
+    "AVAILABLE",
     "LAST_STATUS_SEEN",
     "LAST_SEEN",
     "AGE",
@@ -30,7 +29,7 @@ def replicaset_output_summary(
     ):
         data.append(replicaset_summary_data(replicaset))
     rv = tabulate(
-        sorted(data, key=lambda x: [x[4]]),
+        sorted(data, key=lambda x: [x[5]]),
         headers=SUMMARY_HEADERS,
         tablefmt="plain",
     )
@@ -45,14 +44,23 @@ def replicaset_summary_data(replicaset: Dict) -> List[str]:
     name = meta["name"]
     namespace = meta["namespace"]
     k8s_status = replicaset[lib.BE_K8S_STATUS]
-    desired = k8s_status["desiredNumberScheduled"]
-    current = k8s_status["currentNumberScheduled"]
-    ready = k8s_status["numberReady"]
+    desired = k8s_status["replicas"]
+    if "availableReplicas" in k8s_status:
+        current = k8s_status["availableReplicas"]
+        available = k8s_status["availableReplicas"]
+    else:
+        current = 0
+        available = 0
+    if "readyReplicas" in k8s_status:
+        ready = k8s_status["readyReplicas"]
+    else:
+        ready = 0
     age = lib.calc_age(lib.to_timestamp(meta[lib.METADATA_CREATE_TIME]))
     d_c_r = f"{desired}/{current}/{ready}"
     rv = [
         name,
         d_c_r,
+        available,
         last_status_seen,
         lib.epoch_to_zulu(replicaset["time"]),
         age,
